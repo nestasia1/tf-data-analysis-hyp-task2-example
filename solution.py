9a2ae6472c6911a3d95fd008172f6d0873157604
nestasia1 <109359981+nestasia1@users.noreply.github.com>
2023-04-13 21:13:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 1022895883 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.01
+    statistic, p_value = scipy.stats.ks_2samp(x, y)
+
+    return p_value < alpha
